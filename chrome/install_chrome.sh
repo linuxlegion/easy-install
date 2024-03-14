61f5b3f02fc9d6d5fb9dd290913db58ce40b830a
Herman Singh Umrao <63940157+hermanumrao@users.noreply.github.com>
2024-03-14 15:14:31 +0530

    added chrome installation script

---
+++ chrome/install_chrome.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt-get install -f
+
+sudo apt update
+sudo apt upgrade google-chrome-stable
